bf952ddefd9c410fc0fd38a83aa180da931a7cba
shubham.kumar <shubham.k@clario.com>
2025-06-23 15:16:33 +0530

    Dependency Injection Cons/Setter ,Notes

---
+++ src/main/java/com/dependencyInjection/constructor/Car.java
@@ -0,0 +1,13 @@
+package com.dependencyInjection.constructor;
+
+public class Car {
+    private Specification specification;
+
+    public Car(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void display(){
+        System.out.println("Car Details : " +specification.toString());
+    }
+}
